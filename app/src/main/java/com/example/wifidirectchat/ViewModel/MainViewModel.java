6b75132e58c0c26e070a3891b169250fa6aac456
dbejanishvili <datobejanishvili@gmail.com>
2019-07-25 20:27:19 +0400

    history page view implemented, viewModel and model protorypes created

---
+++ app/src/main/java/com/example/wifidirectchat/ViewModel/MainViewModel.java
@@ -0,0 +1,58 @@
+package com.example.wifidirectchat.ViewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import com.example.wifidirectchat.Model.ChatHistoryEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+    private MutableLiveData<Boolean> searchStatus;
+    private MutableLiveData<List<ChatHistoryEntity>> history;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        searchStatus = new MutableLiveData<>();
+        history = new MutableLiveData<>();
+        List<ChatHistoryEntity> l = new ArrayList<>();
+        //todo add database implementation
+        l.add(new ChatHistoryEntity("bakura","xutshabati",5));
+        l.add(new ChatHistoryEntity("bejana","samshabati",330));
+        l.add(new ChatHistoryEntity("gergili","otxshabati",24));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        l.add(new ChatHistoryEntity("bakura","xutshabati",10));
+        history.setValue(l);
+    }
+
+
+
+    public void startSearch(){
+
+    }
+
+    public void stopSearch(){
+
+    }
+
+    public MutableLiveData<Boolean> getSearchStatus(){
+        return searchStatus;
+    }
+
+    public MutableLiveData<List<ChatHistoryEntity>> getHistory(){
+        return history;
+    }
+
+    public void clearHistory() {
+    }
+}
